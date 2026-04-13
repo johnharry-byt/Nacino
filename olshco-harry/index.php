1092b91e472f5ad188c4083c0d1900de66218ef6
johnharry-byt <johnharrynacino2020@gmail.com>
2026-04-13 22:35:47 +0800

    Add files via upload

---
+++ olshco-harry/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>OLSHCO System</title>
+    <meta http-equiv="refresh" content="0; url=login.html">
+</head>
+<body>
+    <p>Redirecting to login...</p>
+</body>
+</html>
